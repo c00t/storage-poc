efcd5564ff24850b5c2e3c3eefc566d873eb4927
cupofc0t <cupofc0t@gmail.com>
2023-03-02 13:06:53 +0800

    fmt code

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 #![cfg_attr(not(test), no_std)]
-
 //  Language Features
 #![feature(coerce_unsized)]
 #![feature(ptr_metadata)]
 #![feature(unsize)]
-
 //  Library Features
 #![feature(allocator_api)]
 #![feature(layout_for_ptr)]
@@ -13,7 +11,6 @@
 #![feature(nonnull_slice_from_raw_parts)]
 #![feature(slice_ptr_get)]
 #![feature(slice_ptr_len)]
-
 //  Lints
 #![deny(missing_docs)]
 
